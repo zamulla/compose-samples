be70c6395197e3c679843b7099df6c66517d519a
Aleksey Zamulla <karikaturkin@gmail.com>
2025-10-29 15:38:19 +0100

    iOS: the iOS app + iosMain implementations

--- Jetcaster/core/data/src/iosMain/kotlin/di/DataDiModule.ios.kt
+++ Jetcaster/core/data/src/iosMain/kotlin/di/DataDiModule.ios.kt
@@ -2,6 +2,7 @@ package com.example.jetcaster.core.data.di
 
 import androidx.room.RoomDatabase
 import com.example.jetcaster.core.data.database.JetcasterDatabase
+import com.example.jetcaster.core.data.network.IOSOnlineChecker
 import com.example.jetcaster.core.data.network.OnlineChecker
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -12,11 +13,6 @@ actual val platformDataModule: Module = module {
     }
 
     single<OnlineChecker> {
-        object : OnlineChecker {
-            override fun checkIfOnline(): Boolean {
-                // TODO figure out how to check if it's online
-                return true
-            }
-        }
+        IOSOnlineChecker()
     }
 }
