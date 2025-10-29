6ca2868a2141ff7ddf37c11cd00e3c80bda6d40b
Aleksey Zamulla <karikaturkin@gmail.com>
2025-10-29 15:38:19 +0100

    iOS: the iOS app + iosMain implementations

---
+++ Jetcaster/JetcasterMigration/JetcasterMigrationTests/JetcasterMigrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  JetcasterMigrationTests.swift
+//  JetcasterMigrationTests
+//
+//  Created by Aleksey.Zamulla on 29.10.25.
+//
+
+import Testing
+@testable import JetcasterMigration
+
+struct JetcasterMigrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
